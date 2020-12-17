bddf974ce6adb95be3b41ae187a888f89e1778b1
Michael Raguso <michael.raguso@gmail.com>
2020-12-16 22:58:29 -0500

    remove babel svg loader and use webpack

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import NFY from '../public/nfy4.svg';
+import Nfy from '../nfy4.svg';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <NFY />
+      <Nfy />
     </div>
   );
 }
