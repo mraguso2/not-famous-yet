dad56480996be3f64d86a3ee19f60d07aea861bf
Michael Raguso <michael.raguso@gmail.com>
2020-12-16 22:32:17 -0500

    wipe more out and add NFY holding img

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,11 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import NFY from '../public/nfy4.svg';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <p>Test</p>
-        </div>
-      </main>
+      <NFY />
     </div>
   );
 }
